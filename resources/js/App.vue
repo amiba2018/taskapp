caba901668bad921f858752765f7b3565ef20ded
Masato Miyamoto <project-europa@excite.co.jp>
2020-05-27 09:13:24 +0900

    add: SPAのリンクを追加

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <router-view></router-view>
-    </div>
+  <div>
+    <router-link to="/hello">Hello</router-link>
+    <router-link to="/ExampleComponent">Example</router-link>
+
+    <router-view></router-view>
+  </div>
 </template>
 
